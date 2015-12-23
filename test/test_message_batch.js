4b5a27c4005f8f8139679bd8b34bd8ce875469c1
Brandon White <bwhite@uber.com>
2015-12-23 00:00:18 -0800

    more tests for messagesize

--- test/test_message_batch.js
+++ test/test_message_batch.js
@@ -26,34 +26,80 @@ var Buffer = require('buffer').Buffer;
 
 var maxBatchSizeBytes = 100000;
 
-test('MessageBatch can batch several strings', function testMessageBatchString(assert) {
+test('MessageBatch can batch several strings', function testMessageBatchString(assert) { // eslint-disable-line
     var messageBatch = new MessageBatch(maxBatchSizeBytes);
     messageBatch.addMessage('This is a test.');
     messageBatch.addMessage('Foo');
     messageBatch.addMessage('Bar');
     var batchedMessage = messageBatch.getBatchedMessage();
     var numMessages = batchedMessage.readInt32BE(0);
+    var firstMessageSize = batchedMessage.readInt32BE(4);
+    var secondMessageSize = batchedMessage.readInt32BE(23);
+    var thirdMessageSize = batchedMessage.readInt32BE(30);
 
     assert.equal(messageBatch.numMessages, 3);
     assert.equal(messageBatch.sizeBytes, 37);
     assert.equal(numMessages, 3);
+    assert.equal(firstMessageSize, 15);
+    assert.equal(secondMessageSize, 3);
+    assert.equal(thirdMessageSize, 3);
+    assert.equal(batchedMessage.toString(undefined, 8, 23), 'This is a test.');
+    assert.equal(batchedMessage.toString(undefined, 27, 30), 'Foo');
+    assert.equal(batchedMessage.toString(undefined, 34, 37), 'Bar');
+
+    messageBatch.resetBatchedMessage();
+    messageBatch.addMessage('This is a test.');
+    messageBatch.addMessage('Foo');
+    messageBatch.addMessage('Bar');
+    batchedMessage = messageBatch.getBatchedMessage();
+    numMessages = batchedMessage.readInt32BE(0);
+
+    assert.equal(messageBatch.numMessages, 3);
+    assert.equal(messageBatch.sizeBytes, 37);
+    assert.equal(numMessages, 3);
+    assert.equal(firstMessageSize, 15);
+    assert.equal(secondMessageSize, 3);
+    assert.equal(thirdMessageSize, 3);
     assert.equal(batchedMessage.toString(undefined, 8, 23), 'This is a test.');
     assert.equal(batchedMessage.toString(undefined, 27, 30), 'Foo');
     assert.equal(batchedMessage.toString(undefined, 34, 37), 'Bar');
     assert.end();
 });
 
-test('MessageBatch can batch several buffers', function testMessageBatchBuffers(assert) {
+test('MessageBatch can batch several buffers', function testMessageBatchBuffers(assert) { // eslint-disable-line
     var messageBatch = new MessageBatch(maxBatchSizeBytes);
     messageBatch.addMessage(new Buffer('This is a test.'));
     messageBatch.addMessage(new Buffer('Foo'));
     messageBatch.addMessage(new Buffer('FooBar'));
     var batchedMessage = messageBatch.getBatchedMessage();
     var numMessages = batchedMessage.readInt32BE(0);
+    var firstMessageSize = batchedMessage.readInt32BE(4);
+    var secondMessageSize = batchedMessage.readInt32BE(23);
+    var thirdMessageSize = batchedMessage.readInt32BE(30);
 
     assert.equal(messageBatch.numMessages, 3);
     assert.equal(messageBatch.sizeBytes, 40);
     assert.equal(numMessages, 3);
+    assert.equal(firstMessageSize, 15);
+    assert.equal(secondMessageSize, 3);
+    assert.equal(thirdMessageSize, 6);
+    assert.equal(batchedMessage.toString(undefined, 8, 23), 'This is a test.');
+    assert.equal(batchedMessage.toString(undefined, 27, 30), 'Foo');
+    assert.equal(batchedMessage.toString(undefined, 34, 40), 'FooBar');
+
+    messageBatch.resetBatchedMessage();
+    messageBatch.addMessage(new Buffer('This is a test.'));
+    messageBatch.addMessage(new Buffer('Foo'));
+    messageBatch.addMessage(new Buffer('FooBar'));
+    batchedMessage = messageBatch.getBatchedMessage();
+    numMessages = batchedMessage.readInt32BE(0);
+
+    assert.equal(messageBatch.numMessages, 3);
+    assert.equal(messageBatch.sizeBytes, 40);
+    assert.equal(numMessages, 3);
+    assert.equal(firstMessageSize, 15);
+    assert.equal(secondMessageSize, 3);
+    assert.equal(thirdMessageSize, 6);
     assert.equal(batchedMessage.toString(undefined, 8, 23), 'This is a test.');
     assert.equal(batchedMessage.toString(undefined, 27, 30), 'Foo');
     assert.equal(batchedMessage.toString(undefined, 34, 40), 'FooBar');
