84547da0f98305af8a21f2e5218e72898e230cfa
Hongliang Xu <hongxu@uber.com>
2016-12-16 13:43:16 -0800

    At-least-once API

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = require("./lib/kafka_producer");
+module.exports.KafkaDataProducer = require("./lib/kafka_data_producer");
+module.exports.ProducerRecord = require("./lib/producer_record");
